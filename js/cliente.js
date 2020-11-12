22902e96b958f47b06440031d38e52e2a06280d5
Mario <marinhomarqs@gmail.com>
2020-11-12 20:17:37 -0300

    Codigo da Aula 11-11-20

---
+++ js/cliente.js
@@ -0,0 +1,48 @@
+
+
+var button = document.getElementById('requisicao');
+var produtos;
+
+var div = document.getElementById('lista');
+
+
+function getProdutos(event) {
+    console.log('entrou');
+    event.preventDefault();
+    var xhr = new XMLHttpRequest()
+   
+    xhr.open("GET", "http://localhost:8080/produtos", true);
+
+    xhr.onreadystatechange = function(){
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200){
+            console.log(xhr.response);
+            produtos = JSON.parse(xhr.response);
+            
+            
+            for(var cont = 0; cont < produtos.length; cont++){
+                var li1 = document.createElement('li');
+                var li2 = document.createElement('li');
+                var li3 = document.createElement('li');
+                
+                div.appendChild(li1);
+                div.appendChild(li2);
+                div.appendChild(li3);
+               
+                li1.innerText = produtos[cont].nome;
+                li2.innerText = produtos[cont].estoque;
+                li3.innerText = produtos[cont].preco;
+               
+            }
+            
+        }
+    }
+
+    xhr.send()
+}
+    var nomeProduto = document.getElementById('nome');
+
+   
+    
+    
+
+button.addEventListener("click", getProdutos, false);
--- js/index.js
+++ js/index.js
@@ -1,36 +1,29 @@
 
-var button = document.getElementById('btnLogin');
+var button = document.getElementById('logar');
 var username = document.getElementById('username');
 var password = document.getElementById('password');
 
-console.log(button);
-console.log(username);
-console.log(password);
 
-button.preve
 
-const login = '{ "username" = ' + username + ', "password" = ' + password + '}';
-
-function enviarLogin(event, login) {
+function logar(event) {
     event.preventDefault();
-
+   
+  
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'https://oficinadh.herokuapp.com/login', true);
-
-    // Envia a informação do cabeçalho junto com a requisição.
+    xhr.open("POST", "localhost:8080/login", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function() { // Chama a função quando o estado mudar.
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // Requisição finalizada. Faça o processamento aqui.
-            
+    xhr.onreadystatechange = function(){
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200){
+            xhr.response;
         }
     }
-    const loginJson = JSON.stringify(login);
-    xhr.send(loginJson);
-    // xhr.send(new Int8Array()); 
-    // xhr.send(document);
-    
+    const login = '{ "username" : "' + username.value + '", "password" : "' + password.value + '"}';
+    xhr.send(login);
+    window.location.href = 'cliente.html';
 }
 
-button.addEventListener("click", enviarLogin, false);
+
+
+console.log(button);
+
+button.addEventListener("click", logar, false);
